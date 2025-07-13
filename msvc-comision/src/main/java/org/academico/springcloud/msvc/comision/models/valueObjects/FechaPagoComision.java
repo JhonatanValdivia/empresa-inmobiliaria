a78888f7db1542aa7403fa42a034e35b09f50241
LEGION <dmirandaf22_2@unc.edu.pe>
2025-07-13 17:26:35 -0500

    modifiqué el objeto de valor Fecha Pago Comision

--- msvc-comision/src/main/java/org/academico/springcloud/msvc/comision/models/valueObjects/FechaPagoComision.java
+++ msvc-comision/src/main/java/org/academico/springcloud/msvc/comision/models/valueObjects/FechaPagoComision.java
@@ -7,56 +7,59 @@ import java.util.Objects;
 
 @Embeddable
 public class FechaPagoComision {
-    private LocalDate fechaPagoComision;
+    private int dia;
+    private int mes;
+    private int año;
 
-    public FechaPagoComision(){}
-    public FechaPagoComision(LocalDate fechaPagoComision){
-        if(fechaPagoComision==null){
-            throw  new IllegalArgumentException("La fecha no puede ser nula");
-        }
-        if(fechaPagoComision.isAfter(LocalDate.now())){
-            throw new IllegalArgumentException("La fecha no puede estar establecida en un tiempo futuro");
-        }
-
-        this.fechaPagoComision=fechaPagoComision;
-    }
-    //solo get, set no porque es un objeto de valor inmutable
-    public LocalDate getFechaPagoComision(){
-        return fechaPagoComision;
+    public FechaPagoComision() {
     }
 
-    public boolean esHoy(){
-        return fechaPagoComision.isEqual(LocalDate.now());
+    public FechaPagoComision(int dia, int mes, int año) {
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
     }
 
-    public boolean esAntesDe(LocalDate otraFecha){
-        return fechaPagoComision.isBefore(otraFecha);
+    public int getDia() {
+        return dia;
     }
 
-    public boolean esDespuesDe(LocalDate otraFecha){
-        return fechaPagoComision.isAfter(otraFecha);
+    public void setDia(int dia) {
+        this.dia = dia;
     }
-    //equals() y hascode() necesarios para que funcione como Object Value
-    @Override
-    public boolean equals(Object object){
-        if(this==object)
-            return true;
 
-        if(!(object instanceof FechaPagoComision))
-            return false;
-
-        FechaPagoComision fechaPago=(FechaPagoComision) object;
-        return fechaPagoComision.equals(fechaPago.fechaPagoComision);
+    public int getMes() {
+        return mes;
     }
-    @Override
-    public int hashCode(){
-        return Objects.hash(fechaPagoComision);
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
     }
 
     @Override
-    public String toString(){
-        return fechaPagoComision.toString(); // devuelve el valor de la fecha "yyyy-MM-dd"
+    public String toString() {
+        return dia + "/" + mes + "/" + año;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof FechaPagoComision)) return false;
+        FechaPagoComision fecha = (FechaPagoComision) obj;
+        return dia == fecha.dia && mes == fecha.mes && año == fecha.año;
+    }
+
+    @Override
+    public int hashCode() {
+        return dia + mes * 31 + año * 31 * 31;
+    }
 }
 
